2d882acb6452ba29b478428dd0f6263a879cf5a7
Anant Pandey <anant432001@gmail.com>
2024-06-01 00:14:08 +0530

    First commit

---
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -0,0 +1,13 @@
+
+class Animal {
+  constructor(name, legCount) {
+    this.name = name
+    this.legCount = legCount
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs`
+  }
+}
+
+let ani = new Animal("Ani", 4);
+console.log(ani.describe());
---
+++ week-3/01-middlewares/01-requestcount.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+let requestCount = 0;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of requests made to the server in the global
+// requestCount variable
+
+function requestCounter(req, res, next){
+  requestCount++;
+  console.log("requestCount = "+requestCount);
+  next();
+}
+
+app.use(requestCounter);
+
+app.get('/user', function(req, res) {
+  console.log("requestCount = "+requestCount);
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  console.log("requestCount = "+requestCount);
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount', function(req, res) {
+  console.log("requestCount = "+requestCount);
+  res.status(200).json({ requestCount });
+});
+
+module.exports = app;
+
+app.listen(3000);
