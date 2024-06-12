ab7a8f9c285abdca7eab6556634f48236a4a6cfa
Anant Pandey <anant432001@gmail.com>
2024-06-12 13:07:42 +0530

    week 6 useMemo assignment 2 done

---
+++ week-6/1-use-memo/src/All_sentences.jsx
@@ -0,0 +1,13 @@
+const words = ["hi", "my", "name", "is", "for", "to", "random", "word"];
+const TOTAL_LINES = 1000;
+const ALL_WORDS = [];
+for (let i = 0; i < TOTAL_LINES; i++) {
+  let sentence = "";
+  for (let j = 0; j < words.length; j++) {
+    sentence += words[Math.floor(words.length * Math.random())];
+    sentence += " ";
+  }
+  ALL_WORDS.push(sentence);
+}
+
+export default ALL_WORDS;
--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from "react";
+// In this assignment, you will create a component that renders a large list of
+// sentences and includes an input field for filtering these items.
+// The goal is to use useMemo to optimize the filtering process, ensuring the list
+// is only re-calculated when necessary (e.g., when the filter criteria changes).
+// You will learn something new here, specifically how you have to pass more than
+// one value in the dependency array
 
-// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
-// The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
-// You will learn something new here, specifically how you have to pass more than one value in the dependency array
-
-const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
-const TOTAL_LINES = 1000;
-const ALL_WORDS = [];
-for (let i = 0; i < TOTAL_LINES; i++) {
-    let sentence = "";
-    for (let j = 0; j < words.length; j++) {
-        sentence += (words[Math.floor(words.length * Math.random())])
-        sentence += " "
-    }
-    ALL_WORDS.push(sentence);
-}
+import React, { useMemo, useState } from "react";
+import ALL_WORDS from "../All_sentences";
 
 export function Assignment2() {
-    const [sentences, setSentences] = useState(ALL_WORDS);
-    const [filter, setFilter] = useState("");
+  const [sentences, setSentences] = useState(ALL_WORDS);
+  const [filter, setFilter] = useState("");
+  const [count, setCounter] = useState(0);
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+    const filteredSentences = useMemo(() => {
+      console.log("Running Expensive operation");
+      const filtered = sentences.filter((x) => x.includes(filter));
+      return filtered;
+    }, [filter, sentences]);
 
-    return <div>
-        <input type="text" onChange={(e) => {
-            setFilter(e.target.value)
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCounter(count + 1);
+        }}>
+        Counter : {count}
+      </button>
+      <br></br>
+      <br></br>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value);
         }}></input>
-        {filteredSentences.map(word => <div>
-            {word}    
-        </div>)}
+      {filteredSentences.map((word) => (
+        <div>{word}</div>
+      ))}
     </div>
+  );
 }
