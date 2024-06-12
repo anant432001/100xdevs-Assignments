a87655e07211d455ec37630887285706d8ccd41a
Anant Pandey <anant432001@gmail.com>
2024-06-12 12:08:00 +0530

    week 6 useMemo assignment 1 done

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,39 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
-// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
-// Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
+// In this assignment, your task is to create a component that performs an expensive
+// calculation (finding the factorial) based on a user input.
+// Use useMemo to ensure that the calculation is only recomputed when the input
+// changes, not on every render.
 
 export function Assignment1() {
-    const [input, setInput] = useState(0);
-    // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+  const [input, setInput] = useState(0);
+  const [counter, setCounter] = useState(0);
+  // Your solution starts here
+  const expensiveValue = useMemo(() => {
+    console.log("Expensive operation!");
+    let temp = 1;
+    for (let i = 1; i <= input; i++) {
+      temp *= i;
+    }
+    return temp;
+  }, [input]);
+  // Your solution ends here
 
-    return (
-        <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
-            />
-            <p>Calculated Value: {expensiveValue}</p>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <p>Calculated Value: {expensiveValue}</p>
+      <br></br>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}>
+        Counter : {counter}
+      </button>
+    </div>
+  );
 }
